07d7727227188e06b447b78ae652b7f870c52fc6
Shivam Gupta <shivamgupta1830@gmail.com>
2024-07-25 22:18:51 +0530

    Shopping cart functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,12 +16,14 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import store from "./store";
 import { Provider } from "react-redux";
+import CartScreen from "./screens/CartScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
